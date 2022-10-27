bdd1d3b7d13812612809cadecd5bed5a2b7aee09
Sarah <sara.2000year14@gmail.com>
2022-10-27 23:57:30 +0200

    Lesson 9 (Comparable & Comparator)

---
+++ src/Lesson9/Solution/NameCompare.java
@@ -0,0 +1,11 @@
+package Lesson9.Solution;
+
+import java.util.Comparator;
+
+class NameCompare implements Comparator<Movie>
+{
+    public int compare(Movie m1, Movie m2)
+    {
+        return m1.getName().compareTo(m2.getName());
+    }
+}
